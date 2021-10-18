33efa0fb6a438886b890eb1b5c67605a5b8803b8
フルヤミウ <miyamiu19642002@gmail.com>
2021-10-18 17:49:32 +0900

    モーダル途中

--- webapp.js
+++ webapp.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+$(function () {
+    //datepicker処理
+    $('.datepicker').datepicker({
+        // buttonImage: "./common/img/calendar.png",  // カレンダーアイコン画像
+        // buttonText: "カレンダーから選択",  // アイコンホバー時の表示文言
+        // buttonImageOnly: true, // ボタンとして表示
+        // showOn: "both",  // アイコン、テキストボックスどちらをクリックでもカレンダー表示
+        showButtonPanel: true, //閉じるボタンと今日ボタンを表示
+        beforeShow: function (textbox, instance) {
+            $('.appendDatepicker').append($('#ui-datepicker-div'));
+        }
+    });
+    //カレンダーボタンをクリックしたらモーダルウィンドウを表示
+    $('#dpTextbox, .ui-datepicker-trigger').on('click', function () {
+        $('.appendDatepicker').addClass('open');
+    });
+});
