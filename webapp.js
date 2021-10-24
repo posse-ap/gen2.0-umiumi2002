87ba9a03c2e622cf397b1779bbb4907b9aaf484f
フルヤミウ <miyamiu19642002@gmail.com>
2021-10-24 16:31:21 +0900

    week2

--- webapp.js
+++ webapp.js
@@ -1,5 +1,20 @@
 'use strict';
 
+$(document).ready(function () {
+    $('.button').click(function () {
+        $('.modal').show();
+        $('.modal').addClass('.transition');
+
+        // $('.overlay').show();
+    });
+    $('.close-btn').click(function () {
+        $('.modal').hide();
+        $('.modal').addClass('.transition');
+
+        // $('.overlay').hide();
+    });
+});
+
 $(function () {
     //datepicker処理
     $('.datepicker').datepicker({
@@ -18,9 +33,33 @@ $(function () {
     });
 });
 
+// const loading = document.querySelector( '#postbutton' );
+
+// window.addEventListener( 'load', () => {
+//     loading.classList.add( 'hide' );
+// }, false );
+
+
+$(document).ready(function () {
+    $('#postbutton').click(function () {
+        $('.loading').show();
+        $('.loading').addClass('.transition');
+
+        // $('.overlay').show();
+    });
+    $('.close-btn').click(function () {
+        $('.loading').hide();
+        $('.loading').addClass('.transition');
+
+        // $('.overlay').hide();
+    });
+});
+
+
 // function(click, => {
 //     let
 // })
 
 let postbutton = document.getElementById('post')
+// ?
 
