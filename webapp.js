133a83f0a2e9c692926aea063ab0df3da0bf8706
フルヤミウ <miyamiu19642002@gmail.com>
2021-11-07 17:32:31 +0900

    phase1終了

--- webapp.js
+++ webapp.js
@@ -1,19 +1,19 @@
 'use strict';
 
-$(document).ready(function () {
-    $('.button').click(function () {
+$(document).ready(function() {
+    $('.button').click(function() {
         $('.modal').show();
         $('.modal').addClass('.transition');
 
         // $('.overlay').show();
     });
-    $('.responsiveButton').click(function () {
+    $('.responsiveButton').click(function() {
         $('.modal').show();
         $('.modal').addClass('.transition');
 
         // $('.overlay').show();
     });
-    $('.close-btn').click(function () {
+    $('.close-btn').click(function() {
         $('.modal').hide();
         $('.modal').addClass('.transition');
 
@@ -21,40 +21,32 @@ $(document).ready(function () {
     });
 });
 
-// r使う////////////
-$(function () {
-    //datepicker処理
-    $('.datepicker').datepicker({
-        showButtonPanel: true, //閉じるボタンと今日ボタンを表示
-        beforeShow: function (textbox, instance) {
-            $('.appendDatepicker').append($('#ui-datepicker-div'));
-        }
-    });
-    //カレンダーボタンをクリックしたらモーダルウィンドウを表示
-    $('#date, .calender').on('click', function () {
-        $('.appendDatepicker').addClass('open');
-    });
-});
-// 使う///
+// 使いたいカレンダー////////////
+// $(function () {
+//     //datepicker処理
+//     $('datepicker').datepicker({
+//         showButtonPanel: true, //閉じるボタンと今日ボタンを表示
+//         beforeShow: function (textbox, instance) {
+//             $('.appendDatepicker').append($('#ui-datepicker-div'));
+//         }
+//     });
+//     //カレンダーボタンをクリックしたらモーダルウィンドウを表示
+//     $('#date, .calender').on('click', function () {
+//         $('.appendDatepicker').addClass('open');
+//     });
+// });
 
 //カレンダー
 
 
-// const loading = document.querySelector( '#postbutton' );
-
-// window.addEventListener( 'load', () => {
-//     loading.classList.add( 'hide' );
-// }, false );
-
-
-$(document).ready(function () {
-    $('#postbutton').click(function () {
+$(document).ready(function() {
+    $('#postbutton').click(function() {
         $('.loading').show();
         $('.loading').addClass('.transition');
 
         // $('.overlay').show();
     });
-    $('.close-btn').click(function () {
+    $('.close-btn').click(function() {
         $('.loading').hide();
         $('.loading').addClass('.transition');
 
@@ -63,213 +55,10 @@ $(document).ready(function () {
 });
 
 
-// function(click, => {
-//     let
-// })
-
+////////////なんだこれ↓
 let postbutton = document.getElementById('post')
-// ?
 
-// ライブラリのロード
-// name:visualization(可視化),version:バージョン(1),packages:パッケージ(corechart)
-// google.load('visualization', '1', { 'packages': ['corechart'] });
 
-// // グラフを描画する為のコールバック関数を指定
-// google.setOnLoadCallback(drawChart);
-
-// // グラフの描画   
-// function drawChart() {
-
-//     // 配列からデータの生成
-//     var data = google.visualization.arrayToDataTable([
-//         ['年度', '所得税', '法人税', '消費税'],
-//         ['H19年度', 16.08, 14.74, 10.27],
-//         ['H20年度', 14.99, 10.01, 9.97],
-//         ['H21年度', 12.91, 6.36, 9.81],
-//         ['H22年度', 12.98, 8.97, 10.03],
-//         ['H23年度', 13.48, 9.35, 10.19],
-//         ['H24年度', 13.99, 9.76, 10.35],
-//         ['H25年度', 15.53, 10.49, 10.83]
-//     ]);
-
-//     // オプションの設定
-//     // var options = {
-//     //     title: '租税の年間推移 ( 単位：兆円 )',
-//     // };
-//     // 指定されたIDの要素に棒グラフを作成
-//     var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
-
-//     // グラフの描画
-//     chart.draw(data);
-// }
-
-// ▼グラフの中身
-// var barChartData = {
-// data: {
-//     labels: ["2", "4", "6", "8", "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30"],
-//     datasets: [
-//         {
-//             fillColor: "rgba(240,128,128,0.6)",    // 塗りつぶし色
-//             strokeColor: "rgba(240,128,128,0.9)",  // 枠線の色
-//             highlightFill: "rgba(255,64,64,0.75)",  // マウスが載った際の塗りつぶし色
-//             highlightStroke: "rgba(255,64,64,1)",   // マウスが載った際の枠線の色
-//             data: [1, 30, 40, 45, 60, 70, 80]     // 各棒の値(カンマ区切りで指定)
-//         },
-//     ]
-// },
-// options: {                       // オプション
-//     responsive: false,  // canvasサイズ自動設定機能を使わない。HTMLで指定したサイズに固定
-//     title: {                           // タイトル
-//         display: true,                     // 表示設定
-//         fontSize: 18,                      // フォントサイズ
-//         fontFamily: "sans-serif",
-//         text: 'タイトル'                   // タイトルのラベル
-//     },
-//     legend: {                          // 凡例
-//         display: false                     // 表示の有無
-//         // position: 'bottom'              // 表示位置
-//     },
-//     scales: {                          // 軸設定
-//         xAxes: [                           // Ｘ軸設定
-//             {
-//                 display: true,                // 表示の有無
-//                 barPercentage: 0.8,           // カテゴリ幅に対する棒の幅の割合
-//                 //categoryPercentage: 0.8,    // 複数棒のスケールに対する幅の割合
-//                 scaleLabel: {                 // 軸ラベル
-//                     display: true,                // 表示設定
-//                     labelString: '横軸ラベル',    // ラベル
-//                     fontColor: "red",             // 文字の色
-//                     fontSize: 16                  // フォントサイズ
-//                 },
-//                 gridLines: {                   // 補助線
-//                     display: false               // 補助線なし
-//                 },
-//                 ticks: {                      // 目盛り
-//                     fontColor: "red",             // 目盛りの色
-//                     fontSize: 14                  // フォントサイズ
-//                 },
-//             }
-//         ],
-//         yAxes: [                           // Ｙ軸設定
-//             {
-//                 display: true,                 // 表示の有無
-//                 scaleLabel: {                  // 軸ラベル
-//                     display: true,                 // 表示の有無
-//                     labelString: '縦軸ラベル',     // ラベル
-//                     fontFamily: "sans-serif",      // フォントファミリー
-//                     fontColor: "blue",             // 文字の色
-//                     fontSize: 16                   // フォントサイズ
-//                 },
-//                 gridLines: {                   // 補助線
-//                     //display: false               // 補助線なし
-//                     color: "rgba(0, 0, 255, 0.2)", // 補助線の色
-//                     zeroLineColor: "black"         // y=0（Ｘ軸の色）
-//                 },
-//                 ticks: {                       // 目盛り
-//                     min: 0,                        // 最小値
-//                     max: 25,                       // 最大値
-//                     stepSize: 5,                   // 軸間隔
-//                     fontColor: "blue",             // 目盛りの色
-//                     fontSize: 14                   // フォントサイズ
-//                 },
-//             },
-//         ],
-//     },
-// }
-// var barChartData = {
-//     // function myBar () {
-//     var ctx = document.getElementById("棒グラフ表示場所").getContext('2d');
-//     var myChart = new Chart(ctx, {
-
-//         type: "bar",    // ★必須　グラフの種類
-//         data: {
-//             labels: ["00年", "05年", "10年", "15年", "20年"],  // Ｘ軸のラベル
-//             datasets: [
-//                 {
-//                     label: "系列Ａ",                            // 系列名
-//                     data: [10, 20, 5, 15, 10],                 // ★必須　系列Ａのデータ
-//                     backgroundColor: "yellow",                  // 棒の塗りつぶし色
-//                     borderColor: "red",                         // 棒の枠線の色
-//                     borderWidth: 2                              // 枠線の太さ
-//                 }
-//             ]
-//         },
-
-//         options: {                       // オプション
-//             responsive: false,  // canvasサイズ自動設定機能を使わない。HTMLで指定したサイズに固定
-//             title: {                           // タイトル
-//                 display: true,                     // 表示設定
-//                 fontSize: 18,                      // フォントサイズ
-//                 fontFamily: "sans-serif",
-//                 text: 'タイトル'                   // タイトルのラベル
-//             },
-//             legend: {                          // 凡例
-//                 display: false                     // 表示の有無
-//                 // position: 'bottom'              // 表示位置
-//             },
-//             scales: {                          // 軸設定
-//                 xAxes: [                           // Ｘ軸設定
-//                     {
-//                         display: true,                // 表示の有無
-//                         barPercentage: 0.8,           // カテゴリ幅に対する棒の幅の割合
-//                         //categoryPercentage: 0.8,    // 複数棒のスケールに対する幅の割合
-//                         scaleLabel: {                 // 軸ラベル
-//                             display: true,                // 表示設定
-//                             labelString: '横軸ラベル',    // ラベル
-//                             fontColor: "red",             // 文字の色
-//                             fontSize: 16                  // フォントサイズ
-//                         },
-//                         gridLines: {                   // 補助線
-//                             display: false               // 補助線なし
-//                         },
-//                         ticks: {                      // 目盛り
-//                             fontColor: "red",             // 目盛りの色
-//                             fontSize: 14                  // フォントサイズ
-//                         },
-//                     }
-//                 ],
-//                 yAxes: [                           // Ｙ軸設定
-//                     {
-//                         display: true,                 // 表示の有無
-//                         scaleLabel: {                  // 軸ラベル
-//                             display: true,                 // 表示の有無
-//                             labelString: '縦軸ラベル',     // ラベル
-//                             fontFamily: "sans-serif",      // フォントファミリー
-//                             fontColor: "blue",             // 文字の色
-//                             fontSize: 16                   // フォントサイズ
-//                         },
-//                         gridLines: {                   // 補助線
-//                             //display: false               // 補助線なし
-//                             color: "rgba(0, 0, 255, 0.2)", // 補助線の色
-//                             zeroLineColor: "black"         // y=0（Ｘ軸の色）
-//                         },
-//                         ticks: {                       // 目盛り
-//                             min: 0,                        // 最小値
-//                             max: 25,                       // 最大値
-//                             stepSize: 5,                   // 軸間隔
-//                             fontColor: "blue",             // 目盛りの色
-//                             fontSize: 14                   // フォントサイズ
-//                         },
-//                     }
-//                 ],
-//             },
-//             layout: {                          // 全体のレイアウト
-//                 padding: {                         // 余白
-//                     left: 0,
-//                     right: 0,
-//                     top: 0,
-//                     bottom: 0
-//                 }
-//             }
-//         }
-//     });
-// }
-// // }
-// // ▼上記のグラフを描画するための記述
-// window.onload = function () {
-//     var ctx = document.getElementById("chart_div").getContext("2d");
-//     window.myBar = new Chart(ctx).Bar(barChartData);
-// }
 
 //棒グラフ
 google.charts.load('current', { packages: ['corechart', 'bar'] });
@@ -316,21 +105,21 @@ function drawBasic() {
     ]);
 
     var options = {
-        chartArea: { left: 30, top: 20, width: '100%', height: '75%' }, 
+        chartArea: { left: 30, top: 20, width: '100%', height: '75%' },
         hAxis: {
             ticks: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30],
             viewWindow: {
                 min: 0,
                 max: 32
             },
-            gridlines: {color: 'none'},
-            textStyle: {color: '#b8cddf'},
+            gridlines: { color: 'none' },
+            textStyle: { color: '#b8cddf' },
         },
         vAxis: {
-            gridlines: {color: 'none'},
+            gridlines: { color: 'none' },
             format: '#h',
             ticks: [0, 2, 4, 6, 8, 10],
-            textStyle: {color: '#b8cddf'},
+            textStyle: { color: '#b8cddf' },
         }
     };
 
@@ -339,14 +128,11 @@ function drawBasic() {
     chart.draw(data, options);
 }
 
-//再描画
-window.onresize = function () {
-    drawBasic();
-}
 
 //円グラフ
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChartLanguage);
+
 function drawChartLanguage() {
     var data = google.visualization.arrayToDataTable([
         ['Contents', 'Percent'],
@@ -364,8 +150,7 @@ function drawChartLanguage() {
         chartArea: { width: '100%', height: '100%' },
         title: 'My Daily Activities',
         pieHole: 0.5,
-        legend:
-            { position: 'none' },
+        legend: { position: 'none' },
         slices: {
             0: { color: '#0A45EC' },
             1: { color: '#0F71BD' },
@@ -384,6 +169,7 @@ function drawChartLanguage() {
 
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChartContent);
+
 function drawChartContent() {
     var data = google.visualization.arrayToDataTable([
         ['Contents', 'Percent'],
@@ -396,8 +182,7 @@ function drawChartContent() {
         chartArea: { width: '100%', height: '100%' },
         // title: 'My Daily Activities',
         pieHole: 0.5,
-        legend:
-            { position: 'none' },
+        legend: { position: 'none' },
         slices: {
             0: { color: '#0A45EC' },
             1: { color: '#0F71BD' },
@@ -411,3 +196,48 @@ function drawChartContent() {
     var chart = new google.visualization.PieChart(document.getElementById('donutchart-content'));
     chart.draw(data, options);
 }
+
+//再描画
+window.onresize = function() {
+    drawBasic();
+    drawChartLanguage();
+    drawChartContent();
+}
+
+
+
+//tweet画面遷移（完）完
+
+
+// ローディング画面
+// const loading = document.querySelector('.loading-area');これ必要？
+let post = document.getElementById('TWEET');
+
+function disappear() {
+    circle.style.display = 'none';
+}
+
+function complete() {
+    loading.style.display = 'block';
+}
+
+function twitter() {
+    let tweetArea = document.getElementById('tweet');
+    // post.addEventListener('click', () => {
+    if (tweetArea.checked) {
+        window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(document.getElementById('tweetBox').value), null)
+    }
+    // })
+}
+
+
+post.addEventListener('click', function(event) {
+    event.preventDefault();
+    // document.getElementById('circle').style.display='block'
+    circle.style.display = 'block';
+    setTimeout(disappear, 3000);
+    setTimeout(complete, 3000);
+    setTimeout(twitter, 3000);
+
+    // circle.classList.add('hide');
+}, false);
