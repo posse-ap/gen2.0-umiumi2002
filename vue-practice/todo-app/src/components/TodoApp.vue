6bf623dda9bf5b822b2abea9540b787a9c9767a3
フルヤミウ <miyamiu19642002@gmail.com>
2023-03-16 23:05:21 +0900

    環境構築

---
+++ vue-practice/todo-app/src/components/TodoApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
